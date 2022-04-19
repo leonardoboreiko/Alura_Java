c8211287594e390e13e4f992dffa49bdb694ff46
leonardoboreiko <leoboreiko@gmail.com>
2022-04-19 16:51:02 -0300

    commit

--- Alura/Java_Basico/POO/bytebank/src/bytebank/Conta.java
+++ Alura/Java_Basico/POO/bytebank/src/bytebank/Conta.java
@@ -1,9 +1,13 @@
 package bytebank;
 
 public class Conta {
-	double saldo;
-	int agencia;
-	int numero;
-	String titular;
-	
+    double saldo;
+    int agencia;
+    int numero;
+    String titular; 
+
+    public void deposita(double valor) {
+        this.saldo = this.saldo + valor; 
+
+    }
 }
